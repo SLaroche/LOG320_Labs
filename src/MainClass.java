e1167a0f9a7a8d8b2f415a21424f3d1b6b256460
am92570 <am92570@logti-a3412-55>
2016-09-14 16:59:17 -0400

    Commit loading files

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,12 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		String chaine1 = "";
-		String chaine2 = "";
 		
+		ArrayList<String> wordsList = getListFile(args[0]);
+		ArrayList<String> dictList = getListFile(args[1]);	
+		System.out.println("ff"+dictList.get(0));
+	}
+	
+	@SuppressWarnings("resource")
+	private static ArrayList<String> getListFile(String filePath)
+	{
+		Scanner textInFile;
+		ArrayList<String> list= new ArrayList<String>();
 		
-		
+		try {
+			textInFile = new Scanner(new File(filePath)).useDelimiter(",\\s*");
+			while (textInFile.hasNextLine()) {
+				list.add(textInFile.nextLine());
+			    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(list);
+		return list;
 	}
 
 }
