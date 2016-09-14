829bd90187e92a131f1d908d8b9be201888c2a64
Laroche <am43290@logti-a3412-58.ens.ad.etsmtl.ca>
2016-09-14 17:02:50 -0400

    add pdf algo and timer

--- src/algo/AnagramAlgo.java
+++ src/algo/AnagramAlgo.java
@@ -1,5 +1,22 @@
 package algo;
 
+import java.text.DecimalFormat;
+
 public abstract class AnagramAlgo {
 
+	
+	protected abstract void run();
+	
+	public void start() {
+		//reference http://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
+		System.out.println("start");
+		long startTime = System.nanoTime();
+		this.run();
+		long endTime = System.nanoTime();
+
+		DecimalFormat formatter = new DecimalFormat("#.000000000");
+		String duration = formatter.format((endTime - startTime));
+		System.out.println("Temps d'exécution : " + duration + " secondes");
+	}
+	
 }
--- src/algo/PdfAlgo.java
+++ src/algo/PdfAlgo.java
@@ -2,4 +2,35 @@ package algo;
 
 public class PdfAlgo extends AnagramAlgo{
 
+	@Override
+	public void run() {
+		boolean test = algo("test", "tset");
+		System.out.println(test);
+	}
+	
+	public boolean algo(String c1, String c2) {
+		c1 = c1.replace(" ", "");
+		c2 = c2.replace(" ", "");
+		
+		System.out.println("start loop " + c1 + " " + c2);
+		for (int i = 0; i < c1.length(); i++) {
+			boolean trouve = false;
+			for (int j = 0; j < c2.length() && trouve == false; j++) {
+				char c = c2.charAt(j);
+				if (c1.charAt(i) == c) {
+					c2 = c2.replaceFirst(c + "", "");
+					trouve = true;
+				}
+			}
+			if (!trouve) {
+				System.out.println(c2 + "");
+				return false;
+			}
+		}
+		System.out.println(c2 + "");
+		if (c2 != "") {
+			return false;
+		}
+		return true;
+	}
 }
