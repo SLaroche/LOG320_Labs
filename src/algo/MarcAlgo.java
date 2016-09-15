47d3fa08e1981ed40f16b348c7a7088afbdb037c
wmarc04 <wmarc-antoine@hotmail.com>
2016-09-15 15:46:41 -0400

    Version 2 - Algo Marc (lire et boucler une seule fois le ficher word.txt sans utiliser une liste)

--- src/algo/MarcAlgo.java
+++ src/algo/MarcAlgo.java
@@ -7,8 +7,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Logique : recuper les mots et les trier en ordre alphabetique les 
- * lettres et ensuite simplement comparer les 2 string.
+ * Logique : 
+ * 
+ * 		- Lire et boucler une seule fois dans la liste des words
+ * 		- Verifier la longueur des mots
+ * 		- Trier les lettres en ordre alphabetique
+ * 		- Simplement comparer les deux String
+ * 
+ * Etape :
+ * 1 - recupere les mots du fichier dict.txt afin de les ajouter dans une liste
+ * 2 - Lire le document word.txt mot par mot
+ * 3 - Prend le mot et boucle dans la liste dict
+ * 4 - Applique l'algo (trier en ordre alphabetique les 
+ * lettres et ensuite simplement comparer les 2 string.)
+ * 5 - affiche le nombre d anagramme pour le mot lu
+ * 6 - Refaire les etapes precedent, jusqu au moment ou tout les mots sont lus
+ * 7 - Afficher le nombre total d anagramme trouve
  * 
  * @author Marc-Antoine
  *
@@ -20,50 +34,53 @@ public class MarcAlgo extends AnagramAlgo {
 	protected void run() {
 		// TODO Auto-generated method stub
 		// Recupere et lire les fichier
-		ArrayList<String> wordsList = getListFile("src/input/words.txt");
+		//ArrayList<String> wordsList = getListFile("src/input/words.txt");
 		ArrayList<String> dictList = getListFile("src/input/dict.txt");
 
 		// Applique les algos
-		parcourtList(wordsList, dictList);
+		algo(dictList, "src/input/words.txt");
 	}
 
 	/**
-	 * Parcourt les listes afin d'appliquer les algo
+	 * Lire le ficher word.txt et parcourt la liste de dictionnaire 
+	 * afin d'appliquer les algo
 	 * 
-	 * @param wordsList
-	 *            : liste de mot (words.txt)
 	 * @param dictList
 	 *            : liste du dicitionnaire (dict.txt)
+	 * @param wordPath : le chemin du fichier word
 	 */
-	public void parcourtList(ArrayList<String> wordsList, ArrayList<String> dictList) {
-		// Variable pour le nombre total d'anagrammes
+	public void algo(ArrayList<String> dictList, String wordPath){
+		Scanner textInFile;
 		int totalCp = 0;
-		// Parcourt tout les mots
-		for (int i = 0; i < wordsList.size(); i++) {
-			int cp = 0;
-			String theWord = wordsList.get(i).replace(" ", "");
+		//ArrayList<String> list = new ArrayList<String>();
 
-			// Parcout tout les mots du dictionnaire
-			for (int j = 0; j < dictList.size(); j++) {
-				String theWordDict = dictList.get(j).replace(" ", "");
-				
-				//Verifie la longueur des mots
-				if (theWord.length() == theWordDict.length()) {
-					// Appelle la methode qui compare les deux mots
-					boolean estAnagramme = isAnagram(theWord, theWordDict);
-					// Si les deux mots sont des anagrammes ont ajoute 1 au
-					// compteur
-					if (estAnagramme)
-						cp++;
+		try {
+			textInFile = new Scanner(new File(wordPath)).useDelimiter(",\\s*");
+			while (textInFile.hasNextLine()) {
+				String theWord = textInFile.nextLine().replace(" ", "");
+				int cp = 0;
+				for (int j = 0; j < dictList.size(); j++) {
+					String theWordDict = dictList.get(j).replace(" ", "");
+					
+					//Verifie la longueur des mots
+					if (theWord.length() == theWordDict.length()) {
+						// Appelle la methode qui compare les deux mots
+						boolean estAnagramme = isAnagram(theWord, theWordDict);
+						// Si les deux mots sont des anagrammes ont ajoute 1 au
+						// compteur
+						if (estAnagramme)
+							cp++;
+					}
 				}
+				totalCp += cp;
+				// Affiche le message pour tout les mots de la liste words
+				System.out.println("Il y a " + cp + " anagrammes pour le mot " + theWord);
 			}
-			totalCp += cp;
-			// Affiche le message pour tout les mots de la liste words
-			System.out.println("Il y a " + cp + " anagrammes pour le mot " + theWord);
+			System.out.println("Il y a un total de " + totalCp + " anagrammes");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		// Affiche le message, a la fin, qui affiche le nombre total d anagramme
-		// trouve
-		System.out.println("Il y a un total de " + totalCp + " anagrammes");
 	}
 
 	/**
@@ -76,8 +93,8 @@ public class MarcAlgo extends AnagramAlgo {
 	 * @return
 	 */
 	public boolean isAnagram(String firstWord, String secondWord) {
-		char[] word1 = firstWord.replaceAll("[\\s]", "").toCharArray();
-		char[] word2 = secondWord.replaceAll("[\\s]", "").toCharArray();
+		char[] word1 = firstWord.replaceAll("[\\s]", "").toLowerCase().toCharArray();
+		char[] word2 = secondWord.replaceAll("[\\s]", "").toLowerCase().toCharArray();
 		Arrays.sort(word1);
 		Arrays.sort(word2);
 		return Arrays.equals(word1, word2);
