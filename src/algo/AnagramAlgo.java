0ffb280b3d435dc3793be629d3831324217516d8
Samuel Croteau <SCroteau94@gmail.com>
2016-09-21 15:18:21 -0400

    correction timer parent

--- src/algo/AnagramAlgo.java
+++ src/algo/AnagramAlgo.java
@@ -1,16 +1,21 @@
+//reference http://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
+
 package algo;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public abstract class AnagramAlgo {
-	String dictPath;
-	String wordsPath;
-	int nbTest = 1;
+	private String dictPath;
+	private String wordsPath;
+	protected long startTime = 0;
+	protected String[] dictArray;
+	protected String[] wordsArray;
 	
 	protected abstract void run();
 	
@@ -21,37 +26,46 @@ public abstract class AnagramAlgo {
 		this.dictPath = dictPath;
 		this.wordsPath = wordsPath;
 		
-		//reference http://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
+		ArrayList<String> dictArrayL = getListFile(dictPath);
+		ArrayList<String> wordsArrayL = getListFile(wordsPath);
+		
+		dictArray = dictArrayL.toArray(new String[0]);
+		wordsArray = wordsArrayL.toArray(new String[0]);
+		
+		startTimer();
 		System.out.println("start");
-		for(int i=0;i<nbTest;i++){
-			this.run();
-		}
+		this.run(dictArray,wordsArray);
+		endTimer();
 	}
 	public void startTimer(){
+		startTime = System.nanoTime();
+	}
+	public void endTimer(){
 		DecimalFormat formatter = new DecimalFormat("0.000000000");;
 		String durationStr;
 		
 		//reference http://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
-		long totalTestDuration = 0;
 		long duration;
-		for(int i=0;i<nbTest;i++){
-			long startTime = System.nanoTime();
-			long endTime = System.nanoTime();
-			duration = (endTime - startTime);
-			totalTestDuration += duration * (1.0/nbTest);
-			durationStr = formatter.format((duration)/1000000000d);
-			System.out.println("test # "+ (i+1) + " Temps d'exécution : " + durationStr + " secondes");
-		}
-		durationStr = formatter.format((totalTestDuration)/1000000000d);
-		System.out.println("Moyenne Temps d'exécution : " + durationStr + " secondes");
+		long startTime = System.nanoTime();
+		//
+		long endTime = System.nanoTime();
+		duration = (endTime - startTime);
+		durationStr = formatter.format((duration)/1000000000d);
+		System.out.println("Temps d'exécution : " + durationStr + " secondes");
 	}
 	
-	private static ArrayList<String> getListFile(String filePath) throws IOException {
+	private static ArrayList<String> getListFile(String filePath) {
 		ArrayList<String> list = new ArrayList<String>();
 		String a;
-		BufferedReader textInFile = new BufferedReader(new FileReader(new File(filePath)));
-		while ((a=textInFile.readLine()) != null) {
-			list.add(a.replaceAll(" ", ""));
+		BufferedReader textInFile;
+		try {
+			textInFile = new BufferedReader(new FileReader(new File(filePath)));
+		
+			while ((a=textInFile.readLine()) != null) {
+				list.add(a.replaceAll(" ", ""));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return list;
 	}
