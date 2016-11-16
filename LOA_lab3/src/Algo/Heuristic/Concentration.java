fe562a40e5692a6e6ccff30af4a48e4c2aae249d
Samuel Croteau <SCroteau94@gmail.com>
2016-11-16 15:29:20 -0500

    debut center of mass

--- LOA_lab3/src/Algo/Heuristic/Concentration.java
+++ LOA_lab3/src/Algo/Heuristic/Concentration.java
@@ -1,6 +1,7 @@
 package Algo.Heuristic;
 
 import util.GameState;
+import util.Pos2D;
 
 public class Concentration implements HeuristicInterface{
 
@@ -8,5 +9,10 @@ public class Concentration implements HeuristicInterface{
 	public int getScore(GameState gameState) {
 		return 0;
 	}
-
+	
+	private Pos2D getCenterOfMass(){
+		
+		
+		return null;
+	}
 }
