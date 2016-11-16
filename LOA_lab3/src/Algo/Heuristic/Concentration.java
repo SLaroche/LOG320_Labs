38837ad9b3c48d93938fc1a241de6b0f4ed836fa
Samuel Croteau <SCroteau94@gmail.com>
2016-11-16 15:05:36 -0500

    add Structure for heuristic

---
+++ LOA_lab3/src/Algo/Heuristic/Concentration.java
@@ -0,0 +1,12 @@
+package Algo.Heuristic;
+
+import util.GameState;
+
+public class Concentration implements HeuristicInterface{
+
+	@Override
+	public int getScore(GameState gameState) {
+		return 0;
+	}
+
+}
