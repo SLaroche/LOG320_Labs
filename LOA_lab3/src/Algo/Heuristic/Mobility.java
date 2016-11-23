67bfce225276eabc5a37114c95b21ea34547652d
wmarc04 <wmarc-antoine@hotmail.com>
2016-11-23 15:30:18 -0500

    Fix & Test Mobility Heuristic

--- LOA_lab3/src/Algo/Heuristic/Mobility.java
+++ LOA_lab3/src/Algo/Heuristic/Mobility.java
@@ -16,11 +16,13 @@ public class Mobility implements HeuristicInterface{
 		for(GameState child : childs){
 			
 			String deplacement = child.stringMoveFromParent;
-			Pos2D start = child.pawnPositionToPositionUtility(deplacement.substring(0,2));
-			Pos2D end = child.pawnPositionToPositionUtility(deplacement.substring(2,4));
-			
-			//Get le score en fonction des priorite
-			scoreMobility += findPriority(gameState, child, start, end);
+			if(deplacement != null){
+				Pos2D start = child.pawnPositionToPositionUtility(deplacement.substring(0,2));
+				Pos2D end = child.pawnPositionToPositionUtility(deplacement.substring(2,4));
+				
+				//Get le score en fonction des priorite
+				scoreMobility += findPriority(gameState, child, start, end, PlayerNumber);
+			}
 		}
 		return scoreMobility;
 	}
@@ -33,25 +35,25 @@ public class Mobility implements HeuristicInterface{
 	 * @param end : deplacement fin
 	 * @return retourne le scores
 	 */
-	private float findPriority(GameState parent, GameState child, Pos2D start, Pos2D end){
-		float score = 0;
+	private float findPriority(GameState parent, GameState child, Pos2D start, Pos2D end, int playerNumber){
+		float score = 1;
 		
 		//Capture
 		if(parent.board[end.x][end.y] != 0){
 			//Proche du ledge
 			if(end.x <= 1 || end.x >= parent.board.length-1 || end.y <= 1 || end.y >= parent.board.length-1){
-				score += 1;
+				score = 1;
 			}
 			//coin haut
 			else if(end.x == 0 && (end.y == 0 || end.y == parent.board.length)){
-				score += 0.5;
+				score = 0.5f;
 			}
 			//coin bas
 			else if(end.y == parent.board.length && (end.y == 0 || end.y == parent.board.length)){
-				score += 0.5;
+				score = 0.5f;
 			}
 			else{
-				score +=2;
+				score =2;
 			}
 		}
 
